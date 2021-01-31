dbcf1461936a0b32f03cb34adf9650d240ae06f2
Shen studio <77085634+Shenstudio-max@users.noreply.github.com>
2021-01-31 18:16:52 +0530

    Update and rename shen.sh to shenstudio.sh

--- shenstudio.sh
+++ shenstudio.sh
@@ -8,7 +8,8 @@ clear
 clear
 echo
 figlet THIS TOOL CREATE BY| lolcat -a -d 5
-figlet SHEN STUDIO ▁ ▂ ▄ ▅ ▆ ▇ASANGA ▇ ▆ ▅ ▄ ▂ ▁| lolcat -a -d 5
+figlet SHEN STUDIO | lolcat -a -d 5
+figlet with ASANGA | lolcat -a -d 5
 sleep 1
 clear    
 
