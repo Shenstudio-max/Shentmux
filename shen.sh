10eae44821669d411e0795bf1833bfd2941a5f13
Shen studio <77085634+Shenstudio-max@users.noreply.github.com>
2021-01-31 18:10:14 +0530

    Create shen.sh

---
+++ shen.sh
@@ -0,0 +1,24 @@
+clear
+echo
+figlet SHEN | lolcat -a -d 5
+figlet STUDIO | lolcat -a -d 5
+sleep 1
+clear   
+
+clear
+echo
+figlet THIS TOOL CREATE BY| lolcat -a -d 5
+figlet SHEN STUDIO ▁ ▂ ▄ ▅ ▆ ▇ASANGA ▇ ▆ ▅ ▄ ▂ ▁| lolcat -a -d 5
+sleep 1
+clear    
+
+pkg update
+pkg upgrade
+pkg install git
+
+pkg install tumx
+
+clear
+echo
+figlet SHEN | lolcat -a -d 5
+figlet STUDIO | lolcat -a -d 5
